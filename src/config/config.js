63b8c299dff2705120fcc6ce0239cd1ff6579e5e
lazyshivam <shivamgoswami.ss.pp@gmail.com>
2024-03-25 22:15:17 +0530

    url updated

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,6 @@
-export const Base_Url='http://localhost:8080/v1'
+// export const Base_Url = 'http://localhost:8080/v1'
+export const Base_Url = 'https://job-post-website-anchors-assignment.onrender.com/v1'
+
+
+
+
