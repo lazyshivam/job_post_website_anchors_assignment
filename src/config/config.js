c7361b8588d883b5c96c8c72b6f9b0c2de4d53fa
lazyshivam <shivamgoswami.ss.pp@gmail.com>
2024-03-27 08:49:13 +0530

    some changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-// export const Base_Url = 'http://localhost:8080/v1'
-export const Base_Url = 'https://job-post-website-anchors-assignment.onrender.com/v1'
+export const Base_Url = 'http://localhost:8080/v1'
+// export const Base_Url = 'https://job-post-website-anchors-assignment.onrender.com/v1'
 
 
 
